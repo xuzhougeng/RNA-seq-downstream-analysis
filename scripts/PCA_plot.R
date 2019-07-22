f1384add6cf52afe2987e6325d228abae0b0afc2
xuzhougeng <18868192325m0@sina.cn>
2019-07-22 10:01:47 +0800

    change PCA theme

--- scripts/PCA_plot.R
+++ scripts/PCA_plot.R
@@ -3,6 +3,7 @@
 PCA_plot <- function(dds){
   vsd <- DESeq2::vst(dds, blind = FALSE)
   p <- DESeq2::plotPCA(vsd,  intgroup = "condition" ) +
-    ggrepel::geom_label_repel(ggplot2::aes(label= name))
+    ggrepel::geom_label_repel(ggplot2::aes(label= name)) +
+    ggplot2::theme_bw()
   p
 }
